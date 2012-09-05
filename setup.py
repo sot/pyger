7ed04a7834d86cee05807b76ec1bdb4a539da984
Tom Aldcroft <taldcroft@gmail.com>
2012-09-05 15:07:21 -0400

    Change setup.py to use distutils instead of setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-from setuptools import setup
+from distutils.core import setup
 import os
 
 long_description = """
-A lightweight Python version of Liger for calculating allowed dwell times given spacecraft thermal constraints.
+A lightweight Python version of Liger for calculating allowed dwell times given
+spacecraft thermal constraints.
 
-* Uses a Monte-Carlo approach to sample a realistic ensemble of perigee-exit starting temperatures.
+* Uses a Monte-Carlo approach to sample a realistic ensemble of perigee-exit
+  starting temperatures.
 * Generates both typically achievable (50%) and best-case (90%) dwell times.
 * Includes PSMC, Minus-Z and PLINE models.
-* Constraints implemented as Python classes derived from a base ConstraintModel class.
+* Constraints implemented as Python classes derived from a base ConstraintModel
+  class.
 """
 
-__version__ = open(os.path.join(os.path.dirname(__file__), 'pyger/VERSION')).read().strip()
+__version__ = open(os.path.join(os.path.dirname(__file__),
+                                'pyger/VERSION')).read().strip()
 
 setup(name='pyger',
       version=__version__,
@@ -31,6 +35,5 @@ setup(name='pyger',
           'Programming Language :: Python :: 2',
           ],
       packages=['pyger'],
-      include_package_data=True,
-      package_data = {'': ['*.dat', '*.json', 'VERSION']},
+      package_data={'pyger': ['*.dat', '*.json', 'VERSION']},
       )
